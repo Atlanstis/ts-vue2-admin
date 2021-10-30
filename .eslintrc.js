3d6c0a0c8864a9721b30a027785caeee56cc0a77
Gypsophila <futur_@163.com>
2021-10-30 09:48:07 +0800

    eslint 规则说明

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
+  // 使用插件的编码规则
   extends: [
     'plugin:vue/essential',
     'eslint:recommended',
@@ -13,13 +14,17 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
+  // 自定义编码校验规则
+  // 规则配置说明：https://eslint.bootcss.com/docs/user-guide/configuring#configuring-rules
+  // 规则说明：https://eslint.bootcss.com/docs/rules/
+  // vue + typescript 规则说明：https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     //强制使用单引号
     quotes: ['error', 'single'],
     //强制不使用分号结尾
     semi: ['error', 'never'],
-    'space-before-function-paren': 0,
+    'space-before-function-paren': 'off',
   },
 }
