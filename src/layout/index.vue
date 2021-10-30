65600a9740d24a854b20bd9b1040e2f3a470121c
Gypsophila <futur_@163.com>
2021-10-30 11:20:34 +0800

    路由嵌套

---
+++ src/layout/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout">
+    layout
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'LayoutIndex',
+})
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,10 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+import Layout from '@/layout/index.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '',
-    name: 'home',
-    component: () =>
-      import(/* webpackChunkName: 'home'  */ '@/views/home/index.vue'),
-  },
-  {
-    path: '/advert',
-    name: 'advert',
-    component: () =>
-      import(/* webpackChunkName: 'advert'  */ '@/views/advert/index.vue'),
-  },
-  {
-    path: '/advert-space',
-    name: 'advert-space',
-    component: () =>
-      import(/* webpackChunkName: 'home'  */ '@/views/advert-space/index.vue'),
-  },
-  {
-    path: '/course',
-    name: 'course',
-    component: () =>
-      import(/* webpackChunkName: 'course'  */ '@/views/course/index.vue'),
-  },
   {
     path: '/login',
     name: 'login',
@@ -35,28 +12,62 @@ const routes: Array<RouteConfig> = [
       import(/* webpackChunkName: 'login'  */ '@/views/login/index.vue'),
   },
   {
-    path: '/menu',
-    name: 'menu',
-    component: () =>
-      import(/* webpackChunkName: 'menu'  */ '@/views/menu/index.vue'),
-  },
-  {
-    path: '/resourse',
-    name: 'resourse',
-    component: () =>
-      import(/* webpackChunkName: 'resourse'  */ '@/views/resourse/index.vue'),
-  },
-  {
-    path: '/role',
-    name: 'role',
-    component: () =>
-      import(/* webpackChunkName: 'role'  */ '@/views/role/index.vue'),
-  },
-  {
-    path: '/user',
-    name: 'user',
-    component: () =>
-      import(/* webpackChunkName: 'user'  */ '@/views/user/index.vue'),
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () =>
+          import(/* webpackChunkName: 'home'  */ '@/views/home/index.vue'),
+      },
+      {
+        path: '/advert',
+        name: 'advert',
+        component: () =>
+          import(/* webpackChunkName: 'advert'  */ '@/views/advert/index.vue'),
+      },
+      {
+        path: '/advert-space',
+        name: 'advert-space',
+        component: () =>
+          import(
+            /* webpackChunkName: 'home'  */ '@/views/advert-space/index.vue'
+          ),
+      },
+      {
+        path: '/course',
+        name: 'course',
+        component: () =>
+          import(/* webpackChunkName: 'course'  */ '@/views/course/index.vue'),
+      },
+      {
+        path: '/menu',
+        name: 'menu',
+        component: () =>
+          import(/* webpackChunkName: 'menu'  */ '@/views/menu/index.vue'),
+      },
+      {
+        path: '/resourse',
+        name: 'resourse',
+        component: () =>
+          import(
+            /* webpackChunkName: 'resourse'  */ '@/views/resourse/index.vue'
+          ),
+      },
+      {
+        path: '/role',
+        name: 'role',
+        component: () =>
+          import(/* webpackChunkName: 'role'  */ '@/views/role/index.vue'),
+      },
+      {
+        path: '/user',
+        name: 'user',
+        component: () =>
+          import(/* webpackChunkName: 'user'  */ '@/views/user/index.vue'),
+      },
+    ],
   },
   {
     path: '*',
