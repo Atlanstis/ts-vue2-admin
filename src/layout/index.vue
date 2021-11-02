2fcde90662b3a2baf9344dd8695a06bec9c59c77
Gypsophila <futur_@163.com>
2021-11-02 19:30:08 +0800

    路由切换添加动效

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -8,7 +8,9 @@
         <app-header></app-header>
       </el-header>
       <el-main>
-        <router-view></router-view>
+        <transition name="fade-transform" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </el-main>
     </el-container>
   </el-container>
@@ -36,5 +38,22 @@ export default Vue.extend({
 }
 .el-header {
   background: #fff;
+}
+</style>
+
+<style lang="scss">
+.fade-transform-leave-active,
+.fade-transform-enter-active {
+  transition: all 0.5s;
+}
+
+.fade-transform-enter {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.fade-transform-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
